73c621cce3b8f5b248afd0bbe7470cb8964a4610
Bob Loblaw <iwrite@selfdocumentingco.de>
2026-09-18 21:22:59 +0200

    Nice scala

---
+++ scala/nice.scala
@@ -0,0 +1,16 @@
+class Person(val name:String)
+ 
+trait Nice { 
+  def greet() = println("Howdily doodily.")
+}            
+ 
+class Character(override val name:String) extends Person(name) with Nice
+ 
+val flanders = new Character("Ned")
+flanders.greet
+
+object Person {
+    def objectiveGreet() = println("I greet objectively!")
+}
+
+Person.objectiveGreet
